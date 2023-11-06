384bba9e0a62ab2dcc835617fd7346006d447886
張躍 <149373495+ianphoenix@users.noreply.github.com>
2023-11-06 16:36:13 +0800

    Create removeBG.py

---
+++ removeBG.py
@@ -0,0 +1,13 @@
+# 照片去背
+from rembg import remove
+from PIL import Image
+import cv2 
+from google.colab.patches import cv2_imshow
+input_path = 'IMG_5295.png'
+output_path = 'output.png'
+input = Image.open(input_path)
+output = remove(input)
+output.save(output_path)
+
+img = cv2.imread('output.png')
+cv2_imshow(img)
